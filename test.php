85c52861ceed4a3b78ac086652d6920a447ec619
Simon <Gtingwen@outlook.com>
2019-06-26 14:27:22 -0400

    Subject fixed

--- test.php
+++ test.php
@@ -19,15 +19,19 @@ if($emails) {
         /* get information specific to this email */
         $overview = imap_fetch_overview($inbox,$email_number,0);
         $message = imap_fetchbody($inbox,$email_number,2);
+        echo $overview[0]->subject;
         /* output the email header information */
+        /*
         $output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
         $output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
         $output.= '<span class="from">'.$overview[0]->from.'</span>';
         $output.= '<span class="date">on '.$overview[0]->date.'</span>';
         $output.= '</div>';
 
+
         /* output the email body */
-        $output.= '<div class="body">'.$message.'</div>';
+        //$output.= '<div class="body">'.$message.'</div>';
+
     }
     echo $output;
 }
